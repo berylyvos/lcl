75ea75ac228968e663b11a670feb5e8ac9a3b15f
Shingo Gnix <870656738@qq.com>
2023-07-05 21:22:42 +0800

    chore: compile under C++17

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ def build(key, lt):
     make_build_directory()
     gxx_options = [
         "/usr/bin/g++",
-        "-Wall", "-g", "-std=c++11",
+        "-Wall", "-g", "-std=c++17",
         "-I", "./src",
         find_path(key),
         "-o", EXE_DIR + key
