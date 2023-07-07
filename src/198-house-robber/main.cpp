ef298869ea376afd57db213c760d753ab7f75647
Shingo Gnix <870656738@qq.com>
2023-07-07 22:31:16 +0800

    feat(198): [dp] rob-house-i

--- src/198-house-robber/main.cpp
+++ src/198-house-robber/main.cpp
@@ -1,41 +1,43 @@
-#include <cstdio>
-#include <cstring>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-class Solution {
+class Solution2 {
 public:
-    int rob1(vector<int>& nums) {
+    int rob(vector<int>& nums) {
         int n = nums.size();
-        
+
         // f[i]: 一定偷 nums[i]
         // g[i]：一定不偷 nums[i]
-        int f = 0, g = 0;
+        int f = nums[0], g = 0;
 
-        for (int i = 0; i < n; ++ i) {
-            int last_f = f, last_g = g;
-            f = last_g + nums[i];
-            g = max(last_f, last_g);
+        for (int i = 1; i < n; ++i) {
+            int pf = f, pg = g;
+            f = nums[i] + pg;
+            g = max(pf, pg);
         }
+        
         return max(f, g);
     }
+};
 
-    int rob2(vector<int>& nums) {
+class Solution {
+public:
+    int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n <= 1) return nums[0];
-        int f[n];
-        memset(f, 0, sizeof f);
-        f[0] = nums[0];
-        f[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < n; ++ i)
-            f[i] = max(f[i - 1], f[i - 2] + nums[i]);
+        if (n <= 1) return nums[0]; 
+        vector<int> f(n);
+        f[0] = nums[0], f[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; ++i) {
+            f[i] = max(nums[i] + f[i - 2], f[i - 1]);
+        }
         return f[n - 1];
     }
 };
 
 int main() {
-    vector<int> a = {1, 2, 3, 1};
-    printf("%d\n", Solution().rob2(a)); // 4
+    vector<int> nums{1, 2, 3, 1};
+    cout << Solution2().rob(nums) << '\n'; // 4
     return 0;
 }
