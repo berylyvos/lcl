5ebe655b765efada49f0f4d37c680af412c6cffc
Shingo Gnix <870656738@qq.com>
2023-07-13 19:12:14 +0800

    refactor(560)

--- src/560-subarray-sum-equals-k/main.cpp
+++ src/560-subarray-sum-equals-k/main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> preSum;
-        int res = 0, s = 0;
-        preSum[0] = 1; // s[-1] = 0
-        for (auto &x: nums) {
+        unordered_map<int, int> cnt;
+        cnt[0] = 1;
+        int s = 0, res = 0;
+        for (int &x: nums) {
             s += x;
-            if (preSum.find(s - k) != preSum.end()) res += preSum[s - k];
-            preSum[s] ++;
+            res += cnt[s - k];
+            cnt[s]++;
         }
-        return res;
+        return res; 
     }
 };
 
