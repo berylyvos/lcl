f02e499bb03b4099a7ae0abbd192756eb7917dd8
Shingo Gnix <870656738@qq.com>
2023-08-08 20:34:34 +0800

    feat(2813): [regret greedy] 357th-week-q4

---
+++ src/2813-maximum-elegance-of-a-k-length-subsequence/main.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <stack>
+#include <unordered_set>
+using namespace std;
+
+// regret greedy
+class Solution {
+    using ll = long long;
+public:
+    long long findMaximumElegance(vector<vector<int>>& items, int k) {
+        int n = items.size();
+        // 利润降序
+        sort(items.begin(), items.end(), [](const auto& a, const auto& b){
+            return a[0] > b[0];
+        });
+
+        ll ans = 0, s = 0;
+        unordered_set<int> category;
+        stack<int> duplicate; // 重复类别的利润
+        for (int i = 0; i < n; ++i) {
+            int p = items[i][0], c = items[i][1];
+            if (i < k) {
+                s += p;
+                if (!category.insert(c).second) duplicate.push(p);
+            } else if (duplicate.size() && category.insert(c).second) {
+                s += p - duplicate.top();
+                duplicate.pop();
+            }
+            ans = max(ans, s + (ll)category.size() * (ll)category.size());
+        }
+
+        return ans;
+    }
+};
