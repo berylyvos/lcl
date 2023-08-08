022c1b6a2c345edba279a47d361c0f6a9d8f2211
Shingo Gnix <870656738@qq.com>
2023-08-08 13:12:39 +0800

    refacor(2812): [bfs + unoin-find]

--- src/2812-find-the-safest-path-in-a-grid/main.cpp
+++ src/2812-find-the-safest-path-in-a-grid/main.cpp
@@ -1,60 +1,62 @@
 #include <vector>
-#include <queue>
+#include <numeric> // iota
 using namespace std;
 
 // https://leetcode.cn/problems/find-the-safest-path-in-a-grid/
 
 // BFS + UnionFind
 class Solution {
-    const int INF = 1e9;
+    using pii = pair<int, int>;
     const int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
 public:
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int n = grid.size();
         if (n == 1) return 0;
-        vector<vector<int>> d(n, vector<int>(n, INF));
-        queue<pair<int, int>> q;
+        vector<vector<int>> d(n, vector<int>(n, -1));
+        vector<pii> q;
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < n; ++j)
                 if (grid[i][j]) {
                     d[i][j] = 0;
-                    q.emplace(i, j);
+                    q.emplace_back(i, j);
                 }
 
+        // 多源bfs
+        vector<vector<pii>> levels {q};
         while (q.size()) {
-            auto [x, y] = q.front(); q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int a = x + dx[i], b = y + dy[i];
-                if (a < 0 || a >= n || b < 0 || b >= n || d[a][b] < INF) continue;
-                d[a][b] = d[x][y] + 1;
-                q.emplace(a, b);
+            vector<pii> nq;
+            int sz = levels.size();
+            for (auto &[x, y]: q) {
+                for (int i = 0; i < 4; ++i) {
+                    int a = x + dx[i], b = y + dy[i];
+                    if (a < 0 || a >= n || b < 0 || b >= n || d[a][b] >= 0) continue;
+                    d[a][b] = sz;
+                    nq.emplace_back(a, b);
+                }
             }
+            levels.emplace_back(nq);
+            q = move(nq);
         }
-
-        int p[n * n];
-        for (int i = 0; i < n * n; ++i) p[i] = i;
-
-        vector<vector<int>> g(2 * n + 1);
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
-                g[d[i][j]].push_back(i * n + j);
         
+        // 并查集
+        vector<int> p(n * n);
+        iota(p.begin(), p.end(), 0);
         function<int(int)> find = [&](int x) {
             if (x != p[x]) return p[x] = find(p[x]);
             return p[x];
         };
 
-        for (int i = g.size() - 1; ~i; --i) {
-            for (int j = 0; j < g[i].size(); ++j) {
-                int x = g[i][j] / n, y = g[i][j] % n;
+        // 倒序枚举答案
+        for (int ans = levels.size() - 2; ~ans; --ans) {
+            for (auto &[x, y]: levels[ans]) {
                 for (int k = 0; k < 4; ++k) {
                     int a = x + dx[k], b = y + dy[k];
                     if (a < 0 || a >= n || b < 0 || b >= n || d[a][b] < d[x][y]) continue;
-                    int pa = find(x * n + y), pb = find(a * n + b);
-                    if (pa != pb) p[pb] = pa;
+                    int p1 = find(x * n + y), p2 = find(a * n + b);
+                    if (p1 != p2) p[p2] = p1;
                 }
             }
-            if (find(0) == find(n * n - 1)) return i;
+            if (find(0) == find(n * n - 1)) return ans;
         }
 
         return 0;
