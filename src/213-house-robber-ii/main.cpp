8a370bceca63a1145c0bc6c5f4ad8f6c1ae231d9
Shingo Gnix <870656738@qq.com>
2023-09-17 10:04:38 +0800

    refactor(213): rob-ii

--- src/213-house-robber-ii/main.cpp
+++ src/213-house-robber-ii/main.cpp
@@ -1,26 +1,26 @@
 #include <cstdio>
 #include <vector>
-
 using namespace std;
+// https://leetcode.cn/problems/house-robber-ii/
 
 class Solution {
 public:
-    int robRange(const vector<int>& nums, int l, int r) {
-        if (l == r) return nums[l];
-        int f = 0, g = 0;
-        for (int i = l; i <= r; ++ i) {
-            int last_f = f, last_g = g;
-            f = nums[i] + last_g;
-            g = max(last_f, last_g);
+    int robRange(vector<int>& a, const int& l, const int& r) {
+        if (l == r) return a[r];
+        int n = r - l + 1;
+        int f = a[l], g = 0, f1;
+        for (int i = l + 1; i <= r; ++i) {
+            f1 = f;
+            f = a[i] + g;
+            g = max(f1, g);
         }
         return max(f, g);
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
         if (n == 1) return nums[0];
-        return max(robRange(nums, 0, n - 2), robRange(nums, 1, n - 1));
+        return max(robRange(nums, 1, n - 1), robRange(nums, 0, n - 2));
     }
 };
 
